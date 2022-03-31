09e5c9c6ad487f004637403cd47edbd813b2306e
haktan-tekin <webhaktan@gmail.com>
2022-03-31 23:46:41 +0300

    nav item vue-link edits

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,16 +1,28 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
-    <a class="navbar-brand" href="#">Ürün Listesi</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-              <a class="nav-link" href="#">Ürün İşlemleri</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Ürün Çıkışı</a>
-          </li>
-      </ul>
-    </div>
+    <ul class="navbar-nav mr-auto">
+      <router-link
+      active-class="active"
+      to="/"
+      tag="a"
+      class="navbar-brand">
+      Ürün Listesi
+      </router-link>
+      <router-link
+      active-class="active"
+      to="/urun-islemleri"
+      tag="li"
+      class="nav-item">
+          <a class="nav-link">Ürün İşlemleri</a>
+      </router-link>
+      <router-link
+      active-class="active"
+      to="/urun-cikisi"
+      tag="li"
+      class="nav-item">
+        <a class="nav-link">Ürün Çıkışı</a>
+      </router-link>
+    </ul>
   </nav>
 </template>
 <script>
