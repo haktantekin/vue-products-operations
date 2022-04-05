4a028b5c2a6e17e6a36ecbeaa7902d4abb4d597b
haktan-tekin <webhaktan@gmail.com>
2022-04-05 13:40:35 +0300

    footer data get on firebase

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,13 +8,15 @@ export const setTradeResult = ({state, commit}, tradeResult) => {
     }
     Vue.http.put("https://urun-islemleri-prod-1b952-default-rtdb.firebaseio.com/trade-result.json", tradeData)
     .then(response =>{
-        console.log(response);
-        console.log(state.purchase);
-        console.log(state.sale);
-        console.log(state.balance);
     })
 }
 
 export const getTradeResult = ({commit}) => {
+
+    Vue.http.get("https://urun-islemleri-prod-1b952-default-rtdb.firebaseio.com/trade-result.json")
+    .then(response =>{
+        console.log(response);
+        commit("updateTradeResult",response.body)
+    })
 
 }
