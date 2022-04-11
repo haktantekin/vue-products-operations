16c1e0e381a8e9d921c65e24031c5d10226f804d
haktan-tekin <webhaktan@gmail.com>
2022-04-11 17:50:12 +0300

    product review with getter

--- src/components/products/ProductSell.vue
+++ src/components/products/ProductSell.vue
@@ -7,28 +7,27 @@
                 <hr>
                 <div class="form-group">
                     <label>Ürün Adı</label>
-                    <select class="form-control" v-model="selectedProduct">
+                    <select class="form-control" v-model="selectedProduct" @change="productSelected">
                         <option 
                          :value="product.key"
                          v-for="product in getProducts">{{product.title}}</option>
                     </select>
                 </div>
-                <div class="card mb-2 border border-danger">
+                <transition name="fade" mode="out-in">
+                <div class="card mb-2 border border-danger" v-if="product !== null">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-12 text-center">
                                 <div class="mb-3">
-                                    <span class="badge badge-info">Stok : 4</span>
-                                    <span class="badge badge-primary">Fiyat : 100,5 TL</span>
+                                    <span class="badge badge-info">Stok : {{product.count}}</span>
+                                    <span class="badge badge-primary">Fiyat : {{product.price | currency}}</span>
                                 </div>
-                                <p class="border border-warning p-2 text-secondary">Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Assumenda debitis deleniti eos impedit iste numquam quos sit.
-                                    Dignissimos, mollitia nemo officia reiciendis repellendus rerum velit. Eos libero magnam
-                                    quas tempore!</p>
+                                <p class="border border-warning p-2 text-secondary">{{product.description}}</p>
                             </div>
                         </div>
                     </div>
                 </div>
+                </transition>
                 <div class="form-group">
                     <label>Adet</label>
                     <input type="text" class="form-control" placeholder="Ürün adetini giriniz..">
@@ -45,16 +44,27 @@ import { mapGetters } from 'vuex'
 export default{
     data(){
         return{
-            selectedProduct:null
+            selectedProduct:null,
+            product : null
         }
     },
     computed: {
         ...mapGetters(["getProducts"])
+    },
+    methods:{
+        productSelected(){
+            console.log(this.selectedProduct);
+            this.product = this.$store.getters.getProduct(this.selectedProduct)[0];
+            console.log(product[0]);
+        }
     }
 }
 </script>
 <style scoped>
      .border-danger {
             border-style: dashed !important;
+        }
+        .badge{
+            color: #000;
         }
 </style>
